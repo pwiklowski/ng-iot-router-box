6867411f0ecf2d4f50bf0462a864dad69a82bcf3
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2019-12-08 16:57:52 +0100

    Update zigbee-herdsman to latest version

--- zigbeeDeviceManager.ts
+++ zigbeeDeviceManager.ts
@@ -1,61 +1,41 @@
-import ZShepherd from "zigbee-herdsman";
+import { Controller } from "zigbee-herdsman";
 
 export default class ZigbeeDeviceManager {
-  shepherd: any;
+  herdsman: any;
   onDeviceUpdate: Function;
   constructor(port: string) {
-    this.shepherd = new ZShepherd(port, {
-      dbPath: "dev.db",
-      sp: {
+    this.herdsman = new Controller({
+      databasePath: "dev.db",
+      serialPort: {
         baudRate: 115200,
-        rtscts: true
+        rtscts: true,
+        path: port
       }
     });
-
-    this.shepherd.on("ready", this.onReady.bind(this));
-    this.shepherd.on("ind", this.onInd.bind(this));
-    this.shepherd.acceptDevIncoming = (devInfo, callback) => {
-      console.log("new device:", devInfo);
-      callback(null, true); //TODO: accept only my devices
-    };
+    this.herdsman.on("deviceJoined", data =>
+      console.log("event", "deviceJoined", data)
+    );
+    this.herdsman.on("message", this.onMessage.bind(this));
   }
 
-  onReady() {
-    this.shepherd.permitJoin(60, function(err) {
+  async onReady() {
+    await this.herdsman.permitJoin(60, function(err) {
       if (err) console.log(err);
     });
-    console.log("me", this.shepherd.info());
-    console.log("list", this.shepherd.list());
     console.log("Server is ready.");
   }
 
-  onInd(msg) {
-    if (msg.type === "devChange") {
+  onMessage(msg) {
+    if (msg.type === "attributeReport") {
       const data = msg.data;
-      const addr = msg.endpoints[0].device.ieeeAddr;
-
-      if (data.cid === "genOnOff") {
-        if (this.onDeviceUpdate) {
-          this.onDeviceUpdate(addr, "closed", data.data.onOff);
-        }
-      } else if (data.cid === "genBasic") {
-        if (this.onDeviceUpdate) {
-          this.onDeviceUpdate(addr, "battery", data.data["65281"][4]);
-        }
-      } else {
-        console.log(
-          "other",
-          data.cid,
-          data.data["65281"][4],
-          JSON.stringify(data)
-        );
-      }
+      const addr = msg.device.ieeeAddr;
+      this.onDeviceUpdate(addr, data);
     }
   }
 
-  start() {
-    this.shepherd.start(function(err) {
-      if (err) console.log(err);
-    });
+  async start() {
+    await this.herdsman.start();
+    console.log("started");
+    await this.onReady();
   }
 }
